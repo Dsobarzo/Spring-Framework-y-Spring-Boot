10b4b5c895f44c145608e2afd04b80ea22ce7fc0
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-06-15 20:38:20 -0400

    arreglo de objetos

--- 2 - Introducción a Spring MVC y Thymeleaf/pringbootthymeleaf/src/main/java/com/dsobarzo/holamundothymeleaf/ControladorInicio.java
+++ 2 - Introducción a Spring MVC y Thymeleaf/pringbootthymeleaf/src/main/java/com/dsobarzo/holamundothymeleaf/ControladorInicio.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Controller
 @Slf4j
 public class ControladorInicio {
@@ -25,11 +28,24 @@ public class ControladorInicio {
         persona.setEmail("diego.sobarzo.o@gmail.com");
         persona.setTelefono("956079568");
 
+        var persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Riquelme");
+        persona2.setEmail("karla.riquelme@gmail.com");
+        persona2.setTelefono("958621258");
+
+        //var personas = new ArrayList();
+        //personas.add(persona);
+        //personas.add(persona2);
+
+        //var personas = Arrays.asList(persona, persona2);
+        var personas = Arrays.asList();
         log.info("Ejecutando el controlador SpringMCV");
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("mensaje2", mensaje2);
         model.addAttribute("saludo", saludo);
         model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 }
