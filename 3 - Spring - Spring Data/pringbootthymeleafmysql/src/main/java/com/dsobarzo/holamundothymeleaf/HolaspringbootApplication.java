01c4c5d3b02b368396182cd11adf25eecfbb4e04
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-06-16 13:27:15 -0400

    .

---
+++ 3 - Spring - Spring Data/pringbootthymeleafmysql/src/main/java/com/dsobarzo/holamundothymeleaf/HolaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dsobarzo.holamundothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaspringbootApplication.class, args);
+	}
+
+}
