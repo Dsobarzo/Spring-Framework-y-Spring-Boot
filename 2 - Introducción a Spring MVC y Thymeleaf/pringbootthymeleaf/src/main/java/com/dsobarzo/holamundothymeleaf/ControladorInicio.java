17ddf61a4065df9a79c32397683293b5d868fa97
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2021-06-15 20:12:28 -0400

    objeto persona

--- 2 - Introducción a Spring MVC y Thymeleaf/pringbootthymeleaf/src/main/java/com/dsobarzo/holamundothymeleaf/ControladorInicio.java
+++ 2 - Introducción a Spring MVC y Thymeleaf/pringbootthymeleaf/src/main/java/com/dsobarzo/holamundothymeleaf/ControladorInicio.java
@@ -1,6 +1,8 @@
 package com.dsobarzo.holamundothymeleaf;
 
+import com.dsobarzo.holamundothymeleaf.domain.Persona;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
+
+    @Value("${index.saludo}")
+    private String saludo;
+
     // "/" Ruta en blanco -> LocalHost
     @GetMapping("/")
     public String inicio(Model model){
         var mensaje = "Hola Mundo Con Thymeleaf";
+        var mensaje2 = "Nuevo mensaje!!!";
+        var persona = new Persona();
+        persona.setNombre("Diego");
+        persona.setApellido("Sobarzo");
+        persona.setEmail("diego.sobarzo.o@gmail.com");
+        persona.setTelefono("956079568");
+
         log.info("Ejecutando el controlador SpringMCV");
         model.addAttribute("mensaje", mensaje);
+        model.addAttribute("mensaje2", mensaje2);
+        model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
         return "index";
     }
 }
---
+++ 2 - Introducción a Spring MVC y Thymeleaf/pringbootthymeleaf/src/main/java/com/dsobarzo/holamundothymeleaf/domain/Persona.java
@@ -0,0 +1,12 @@
+package com.dsobarzo.holamundothymeleaf.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String Apellido;
+    private String Email;
+    private String Telefono;
+
+}
